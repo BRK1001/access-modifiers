703a990f6404c446aae64e3094dc3de9cd21960d
BRK1001 <105447706+BRK1001@users.noreply.github.com>
2023-04-20 23:13:02 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+1.public	The code is accessible for all classes
+
+2.private	The code is only accessible within the same class
+
+3.protected	The code is accessible within the same class,
+            or in a class that is inherited from that class.
+            You will learn more about inheritance in a later chapter
+
+4.internal	The code is only accessible within its own assembly,
+            but not from another assembly. 
+            You will learn more about this in a later chapter.
+
+
+example
+
+public: public string color;
+
+private: 
+using System;
+namespace MyApplication
+{
+    class car
+    {
+        private string model = "maruthi";
+        static void Main(string[] args)
+        {
+            car MyObj=new car(); 
+            Console.WriteLine(MyObj.model);
+        }
+    }
+}
+protected:
